fa3830f8e3f3c94ee421e26ad1e2397ee5080f17
emich-kyle <78440842+emich-kyle@users.noreply.github.com>
2021-04-02 11:28:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ print('The team with the longest name is %s, at %s characters.' % (longest_team_
 
 players_list = [x['name'] for x in statsapi.get('teams',{'sportIds':1,'activeStatus':'Yes','fields':'teams,name'})['teams']]
 print(players_list)
+
+players_list = statsapi.get('sports_players',{'season':2019,'gameType':'R'})['people']
+player_ids = []
+for i in players_list:
+    player_ids.append(i['id'])
+    
+print(player_ids)  ##ok here we go all player ids for 2019 season
+
+import json
+
+players_list = statsapi.get('sports_players',{'season':2019,'gameType':'R'})['people']
+
+with open("test.json", "w") as write_file:
+    json.dump(statsapi.player_stat_data(650556, group="[hitting]", type="yearByYear"), write_file)
